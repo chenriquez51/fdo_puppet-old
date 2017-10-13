91d0fb64c0a5a90a03bfd28e0c1add0d7f83853f
sadminriley <riley@fasterdevops.com>
2017-10-13 11:23:35 -0400

    Initial commit

---
+++ manifests/site.pp
@@ -0,0 +1,44 @@
+# Specific agents defined on top under node declaration
+node 'srv5.riley.science' {    # Specify specific node
+  file {'/root/hello.txt' :
+    ensure  => present,
+    mode    => '0644',
+    content => "THIS IS JUST A TEST XD RAWR",
+  }
+}
+
+node 'srv.fasterdevops.com' {    # Specify specific node
+    include resources
+}
+
+node 'dev.fasterdevops.com' {
+    include resources
+}
+
+node 'silver.fasterdevops.com' {
+    include resources
+}
+# Manage users
+  user { 'kolby':
+    ensure    => present,
+    uid       => '1002',
+    shell     => '/bin/bash',
+    home      => '/home/kolby',
+    managehome => false,
+}
+
+  user { 'riley':
+    ensure    => present,
+    uid       => '1003',
+    shell     => '/bin/bash',
+    home      => '/home/riley',
+    managehome => false,
+}
+
+
+  exec { 'yum update':
+    command   => '/usr/bin/yum update -y'
+}
+# Add files in /etc/puppetlabs/modules/resources/files/
+
+node default {}
---
+++ modules/resources/manifests/init.pp
@@ -0,0 +1,36 @@
+# Basic base configuration for servers
+#
+#
+class resources {
+
+ $base_packages = ['python34', 'git', 'zsh', 'tmux', 'curl', 'whois', 'ntp', 'htop', 'bind-utils']
+
+  file { '/etc/motd' :
+    source    => 'puppet:///modules/resources/motd',
+    mode      => '0644',
+    owner     => 'root',
+ }
+
+  file {'/root/.ssh/authorized_keys' :
+    source    => 'puppet:///modules/resources/riley_sshkey',
+    mode      => '0600',
+    owner     => 'root',
+ }
+
+  file { '/root/.vimrc' :
+    source    => 'puppet:///modules/resources/vimrc',
+    mode      => '0655'
+    owner     => 'root',
+  }
+
+  file { '/opt':
+    ensure    => 'directory',
+    mode      => '0655',
+    owner     =>'root',
+  }
+
+
+  package { $base_packages: ensure => latest, provider => 'yum',}
+
+
+}
