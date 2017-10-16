4f745e2717f353fe983994d5f8bc734c47a044f5
sadminriley <riley@fasterdevops.com>
2017-10-15 20:07:46 -0400

    Added tomcat module

--- modules/resources/manifests/init.pp
+++ modules/resources/manifests/init.pp
@@ -3,7 +3,7 @@
 #
 class resources {
 
- $base_packages = ['python34', 'lsof', 'git', 'zsh', 'tmux', 'curl', 'whois', 'ntp', 'htop', 'bind-utils']
+ $base_packages = ['python34', 'lsof', 'telnet', 'git', 'zsh', 'tmux', 'curl', 'whois', 'ntp', 'htop', 'bind-utils']
 
  $python_packages = ['paramiko', 'fabric', 'pep8', 'pylint']
 
